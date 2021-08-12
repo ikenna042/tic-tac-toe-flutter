79c6b768039a4b8beabdcfd7385bd1834e678e08
IKENNA <ikenna@IKENNAs-MacBook-Pro.local>
2021-08-12 23:03:40 +0100

    Set board tiles

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/board_tile.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,47 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+          body: Center(
+              child: Stack(
+                  children: [Image.asset('images/board.png'), _boardTiles()]))),
     );
+  }
+
+  Widget _boardTiles() {
+    return Builder(builder: (context) {
+      final boardDimension = MediaQuery.of(context).size.width;
+      final titleDimension = boardDimension / 3;
+
+      return Container(
+        width: boardDimension,
+        height: boardDimension,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                BoardTile(dimension: titleDimension),
+                BoardTile(dimension: titleDimension),
+                BoardTile(dimension: titleDimension)
+              ],
+            ),
+            Row(
+              children: [
+                BoardTile(dimension: titleDimension),
+                BoardTile(dimension: titleDimension),
+                BoardTile(dimension: titleDimension)
+              ],
+            ),
+            Row(
+              children: [
+                BoardTile(dimension: titleDimension),
+                BoardTile(dimension: titleDimension),
+                BoardTile(dimension: titleDimension)
+              ],
+            )
+          ],
+        ),
+      );
+    });
   }
 }
---
+++ lib/tile_state.dart
@@ -0,0 +1,1 @@
+enum TileState { EMPTY, CROSS, CIRCLE }
