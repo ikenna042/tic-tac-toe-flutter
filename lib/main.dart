f725fdf26097d10497920c5c7757145e50c8228c
IKENNA <ikenna@IKENNAs-MacBook-Pro.local>
2021-08-15 03:16:35 +0100

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final navigatorKey = GlobalKey<NavigatorState>();
   var _boardState = List.filled(9, TileState.EMPTY);
+  var _currentTurn = TileState.CROSS;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       home: Scaffold(
           body: Center(
               child: Stack(
@@ -32,31 +35,102 @@ class _MyAppState extends State<MyApp> {
         width: boardDimension,
         height: boardDimension,
         child: Column(
-          children: [
-            Row(
-              children: [
-                BoardTile(dimension: titleDimension),
-                BoardTile(dimension: titleDimension),
-                BoardTile(dimension: titleDimension)
-              ],
-            ),
-            Row(
-              children: [
-                BoardTile(dimension: titleDimension),
-                BoardTile(dimension: titleDimension),
-                BoardTile(dimension: titleDimension)
-              ],
-            ),
-            Row(
-              children: [
-                BoardTile(dimension: titleDimension),
-                BoardTile(dimension: titleDimension),
-                BoardTile(dimension: titleDimension)
-              ],
-            )
-          ],
-        ),
+            children: chunk(_boardState, 3).asMap().entries.map((e) {
+          final chunkIndex = e.key;
+          final titleStateChunk = e.value;
+
+          return Row(
+            children: titleStateChunk.asMap().entries.map((innerEntry) {
+              final innerIndex = innerEntry.key;
+              final titleState = innerEntry.value;
+              final titleIndex = (chunkIndex * 3) + innerIndex;
+
+              return BoardTile(
+                tileState: titleState,
+                dimension: titleDimension,
+                onPressed: () => _updateTileStateForIndex(titleIndex),
+              );
+            }).toList(),
+          );
+        }).toList()),
       );
+    });
+  }
+
+  void _updateTileStateForIndex(int indexSelected) {
+    if (_boardState[indexSelected] == TileState.EMPTY) {
+      setState(() {
+        _boardState[indexSelected] = _currentTurn;
+        _currentTurn = _currentTurn == TileState.CROSS
+            ? TileState.CIRCLE
+            : TileState.CROSS;
+      });
+
+      final winner = _findWiner();
+      if (winner != null) {
+        print('The winner is $winner');
+        _showWinnerDialog(winner);
+      }
+    }
+  }
+
+  TileState _findWiner() {
+    TileState Function(int, int, int) winnerForMatch = (a, b, c) {
+      if (_boardState[a] != TileState.EMPTY) {
+        if ((_boardState[a] == _boardState[b]) &&
+            (_boardState[b]) == _boardState[c]) {
+          return _boardState[a];
+        }
+      }
+      return null;
+    };
+
+    final checks = [
+      winnerForMatch(0, 1, 2),
+      winnerForMatch(3, 4, 5),
+      winnerForMatch(6, 7, 8),
+      winnerForMatch(0, 3, 6),
+      winnerForMatch(1, 4, 7),
+      winnerForMatch(2, 5, 8),
+      winnerForMatch(0, 4, 8),
+      winnerForMatch(2, 4, 6),
+    ];
+
+    TileState winner;
+    for (int i = 0; i < checks.length; i++) {
+      if (checks[i] != null) {
+        winner = checks[i];
+        break;
+      }
+    }
+    return winner;
+  }
+
+  void _showWinnerDialog(TileState tileState) {
+    final context = navigatorKey.currentState.overlay.context;
+    showDialog(
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            title: Text('Winner'),
+            content: Image.asset(
+                tileState == TileState.CROSS ? 'images/x.png' : 'images/o.png'),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    _resetGame();
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('New Game'))
+            ],
+          );
+        });
+  }
+
+  void _resetGame() {
+    setState(() {
+      _boardState = List.filled(9, TileState.EMPTY);
+      _currentTurn = TileState.CROSS;
     });
   }
 }
